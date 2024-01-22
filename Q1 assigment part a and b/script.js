0bf7bb72cc196b4d1ae1b81f25b6dcb22616f34a
RohailJavaid786 <155910153+RohailJavaid786@users.noreply.github.com>
2024-01-22 05:19:47 -0800

    Add files via upload

---
+++ Q1 assigment part a and b/script.js
@@ -0,0 +1,25 @@
+
+
+
+//Q1-a//
+
+let age = 25;
+let isStudent = false;
+
+console.log("Age: " + age);
+console.log("Is student: " + isStudent);
+
+
+
+
+
+//Q1-b//
+
+alert("This webpage is designed to provide you with valuable information and a great user experience. Feel free to explore!");
+
+    
+
+
+
+
+
